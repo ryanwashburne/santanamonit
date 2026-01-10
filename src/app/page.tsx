3d80161cd56b0cca651046d369ff95feb4870d35
Ryan Washburne <ryan.washburne@gmail.com>
2026-01-10 12:18:54 -0500

    focus on one page at a time

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,29 @@
 "use client";
 
-import { Box, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 const HomePage = () => {
 	return (
-		<Box mt={32} overflow="hidden">
+		<Box minH="750px">
 			{/* Nicole + Zach text */}
-			<Box textAlign="center" transform="rotate(-8deg)">
-				<Text color="#782e20" fontFamily="cursive" fontSize="8xl">
-					Nicole + Zach
-				</Text>
-			</Box>
+			<Flex justify="center" mt={32}>
+				<Image
+					alt=""
+					height="100%"
+					objectFit="contain"
+					src="/landing.svg"
+					width="800px"
+				/>
+			</Flex>
 
-			{/* Palm trees at bottom */}
-			<Box>
-				{/* Tree 1 */}
-				<Box bottom="0px" left="0%" position="fixed">
-					<Image
-						alt=""
-						height="100%"
-						objectFit="contain"
-						src="/tree-1.svg"
-						width="450px"
-					/>
-				</Box>
-
-				{/* Tree 2 */}
-				<Box bottom="0px" left="20%" position="fixed">
-					<Image
-						alt=""
-						height="100%"
-						objectFit="contain"
-						src="/tree-2.svg"
-						width="500px"
-					/>
-				</Box>
-
-				{/* Tree 3 */}
-				<Box bottom="0px" position="fixed" right="18%">
-					<Image
-						alt=""
-						height="100%"
-						objectFit="contain"
-						src="/tree-3.svg"
-						width="600px"
-					/>
-				</Box>
-
-				{/* Tree 4 */}
-				<Box bottom="0px" position="fixed" right="0px">
-					<Image
-						alt=""
-						height="100%"
-						objectFit="contain"
-						src="/tree-4.svg"
-						width="450px"
-					/>
-				</Box>
+			<Box bottom={0} left={0} position="absolute" right={0}>
+				<Image
+					alt=""
+					height="100%"
+					objectFit="contain"
+					src="/trees.svg"
+					width="100%"
+				/>
 			</Box>
 		</Box>
 	);
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {
+	Box,
 	Link as ChakraLink,
 	Container,
 	Heading,
@@ -12,71 +13,77 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const LINKS = [
-	{
-		label: "Schedule",
-		href: "/schedule",
-	},
+	// {
+	// 	label: "Schedule",
+	// 	href: "/schedule",
+	// },
+	// {
+	// 	label: "Attire",
+	// 	href: "/attire",
+	// },
 	{
 		label: "RSVP",
 		href: "/rsvp",
 	},
-	{
-		label: "Travel",
-		href: "/travel",
-	},
-	{
-		label: "Accommodations",
-		href: "/accommodations",
-	},
-	{
-		label: "Siargao",
-		href: "/siargao",
-	},
-	{
-		label: "Opt-In Activities",
-		href: "/opt-in-activities",
-	},
+	// {
+	// 	label: "Travel",
+	// 	href: "/travel",
+	// },
+	// {
+	// 	label: "Accommodations",
+	// 	href: "/accommodations",
+	// },
+	// {
+	// 	label: "Siargao",
+	// 	href: "/siargao",
+	// },
+	// {
+	// 	label: "Opt-In Activities",
+	// 	href: "/opt-in-activities",
+	// },
 ];
 
 const Header = () => {
 	const pathname = usePathname();
 
 	return (
-		<Container pb={32} pt={16}>
-			<VStack gap={12}>
-				<Link href="/" passHref>
-					<Heading
-						_hover={{
-							opacity: 0.7,
-						}}
-						color="#782E20"
-						fontFamily="header"
-						fontSize="2xl"
-						fontWeight="normal"
-						textTransform="uppercase"
-						transition="opacity 0.3s ease-in-out"
-					>
-						The Monits
-					</Heading>
-				</Link>
-				<HStack fontFamily="heading" fontSize="lg" gap="80px">
-					{LINKS.map((link) => {
-						return (
-							<ChakraLink
-								asChild
-								color="inherit"
-								fontWeight={pathname === link.href ? "bold" : undefined}
-								key={link.href}
-							>
-								<Link href={link.href}>
-									<Text>{link.label}</Text>
-								</Link>
-							</ChakraLink>
-						);
-					})}
-				</HStack>
-			</VStack>
-		</Container>
+		<Box bg="#AAB25F">
+			<Container pb={8} pt={16}>
+				<VStack gap={8}>
+					<Link href="/" passHref>
+						<Heading
+							_hover={{
+								opacity: 0.7,
+							}}
+							color="white"
+							fontFamily="header"
+							fontSize="2xl"
+							fontWeight="normal"
+							textTransform="uppercase"
+							transition="opacity 0.3s ease-in-out"
+						>
+							The Monits
+						</Heading>
+					</Link>
+					<HStack fontFamily="heading" fontSize="lg" gap="80px">
+						{LINKS.map((link) => {
+							return (
+								<ChakraLink
+									asChild
+									color="inherit"
+									fontWeight={pathname === link.href ? "bold" : undefined}
+									key={link.href}
+								>
+									<Link href={link.href}>
+										<Text>{link.label}</Text>
+									</Link>
+								</ChakraLink>
+							);
+						})}
+					</HStack>
+				</VStack>
+			</Container>
+		</Box>
 	);
 };
 
